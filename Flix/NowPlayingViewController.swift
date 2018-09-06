3bbbc1660ef2f266d151afd03f87b13fe3109c2c
Whitney Griffith <whitney.griffith@bison.howard.edu>
2018-09-06 14:16:40 -0400

    Table view implemented

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.rowHeight = 220
+        
+        tableView.dataSource = self
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -32,8 +37,15 @@ class NowPlayingViewController: UIViewController {
             }
         }
         task.resume()
-        
-
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,7 +53,5 @@ class NowPlayingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-
 
 }
