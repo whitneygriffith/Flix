7c5c027fec1a42740af2e2d7c1571b9cb9df6679
Whitney Griffith <whitney.griffith@bison.howard.edu>
2018-09-09 12:02:04 -0400

    CollectionView completed and Detail screen for Collectionview

--- Flix/ViewControllers/SuperheroViewController.swift
+++ Flix/ViewControllers/SuperheroViewController.swift
@@ -61,11 +61,22 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
         task.resume()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UICollectionViewCell
+        if let indexPath = collectionView.indexPath(for: cell){
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+            
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 }
     
