f34c6381b22208634e7adf8961b630f747f5bc4a
Whitney Griffith <whitney.griffith@bison.howard.edu>
2018-09-09 11:44:24 -0400

    Collection View set up for SUperheros

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Whitney Griffith on 9/9/18.
+//  Copyright © 2018 Whitney Griffith. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
---
+++ Flix/ViewControllers/SuperheroViewController.swift
@@ -0,0 +1,71 @@
+//
+//  SuperheroViewController.swift
+//  Flix
+//
+//  Created by Whitney Griffith on 9/9/18.
+//  Copyright © 2018 Whitney Griffith. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies: [[String: Any]] = []
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath ) as! PosterCell
+        let movie = movies[indexPath.item]
+        if let posterPathString = movie["poster_path"] as? String{
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            let posterURL = URL (string: baseURLString + posterPathString)!
+            cell.posterImageView.af_setImage(withURL: posterURL)
+            
+        }
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.dataSource = self
+        fetchMovies()
+    }
+    
+    func fetchMovies(){
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            //This will run when the asynchronous network request returns.
+            if let error = error {
+                print(error.localizedDescription)
+            }else if let data = data {
+                let dataDictionary = try!  JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.collectionView.reloadData()
+                //self.refreshControl.endRefreshing()
+                
+            }
+        }
+        task.resume()
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+    
