ea5a882dbd22c89507370c2d37470f8d7f2ac8ee
Whitney Griffith <whitney.griffith@bison.howard.edu>
2018-09-09 11:01:43 -0400

    Detailed view

---
+++ Flix/ViewControllers/DetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewController.swift
+//  Flix
+//
+//  Created by Whitney Griffith on 9/8/18.
+//  Copyright © 2018 Whitney Griffith. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var backDropImageView: UIImageView!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    var movie: [String: Any]?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        if let movie = movie {
+            titleLabel.text = movie["title"] as? String
+            releaseDateLabel.text = movie["release_date"] as? String
+            overviewLabel.text = movie["overview"] as? String
+            let backdropPathString = movie["backdrop_path"] as! String
+            let posterPathString =  movie["poster_path"] as! String
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            
+            let backdropURL = URL(string: baseURLString + backdropPathString)!
+            backDropImageView.af_setImage(withURL: backdropURL)
+            
+            let posterPathURL = URL(string: baseURLString + posterPathString)!
+            
+            posterImageView.af_setImage(withURL: posterPathURL)
+            
+        }
+        
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
